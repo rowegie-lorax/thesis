c4e9a5f511888ea2f0a32bd35009851e771f7f94
Rowegie <rowegie.lorax@gmail.com>
2017-07-28 21:59:51 +0800

    updated for Pretty Url

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,15 @@
 
     angular
         .module('app', ['ui.router'])
-        .config(config);
+        .config(config)
+        .run(run);
 
     config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+    run.$inject = ['$rootScope', 'LocalStorage' ];
+
 
     function config($stateProvider, $urlRouterProvider, $locationProvider){
+        $locationProvider.html5Mode(true);
     	$locationProvider.hashPrefix('');
     	$stateProvider
     		.state('home', {
@@ -37,6 +41,28 @@
 
     	$urlRouterProvider.otherwise('/home');
     }
+
+
+
+    function run($rootScope, LocalStorage){
+        console.log("runn");
+        $rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
+            console.log('asdf');
+            if( LocalStorage.get('is_logged_in') !== null && toState.name === 'login'){
+                event.preventDefault();
+                
+                $state.go('home');
+            }else{
+                console.log("home");
+            }
+            // if( && !UsersService.getCurrentUser()) {
+            // event.preventDefault();
+            // $state.go('login');
+            // }
+        });
+
+    }
+
 })();
 
 
